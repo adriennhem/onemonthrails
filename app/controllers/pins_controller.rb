3d09eb4559d9628fd74a15d70f43e5cab1e12b0a
Adrien Nhem <adrien.nhem@gmail.com>
2014-06-22 16:16:36 +0200

    Paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -54,4 +54,6 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
+
+    
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+
+ Paperclip.options[:command_path] = "/usr/local/bin/identify"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -36,4 +38,6 @@ Rails.application.configure do
 config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
 end
